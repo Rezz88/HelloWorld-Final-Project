7a8ffac9f2dcbd338cfe35341eecc0c214ebec05
Eric Washburn <cm_washburn@MacBook-Pro-Eric.local>
2018-02-24 15:26:33 -0500

    sign up now sends age and gender

--- FrontEnd/src/views/signUp/index.js
+++ FrontEnd/src/views/signUp/index.js
@@ -8,12 +8,14 @@ class SignUp extends Component {
         this.state = {
             username: '',
             password: '',
+            password1: '',
             email: '',
+            age: '',
+            gender: '',
             loggedIn: false,
             error: ''
         }
     }
-
 
     setInputValue =(key, value)=> {
         this.setState({[key]: value})
@@ -21,7 +23,7 @@ class SignUp extends Component {
 
     signingUp = () =>  {
         console.log('signing up')
-        const { username, password, email} = this.state
+        const { username, password, email, age, gender} = this.state
         fetch('/sign-up', {
             method: 'post',
             credentials: 'include',
@@ -29,11 +31,15 @@ class SignUp extends Component {
                 username: username,
                 password: password,
                 email: email,
+                age: age,
+                gender: gender,
                 loggedIn: true
             })
             })
             .then(x => x.text())
             .then(x => JSON.parse(x))
+            .then(x=> {console.log('this is the response',x); return x})
+            //response fails if there is an error in the sign up
             .then(x => {
             if (x.response===true)  { 
                 this.signUpPass()
@@ -47,18 +53,23 @@ class SignUp extends Component {
     }
 
     signUpPass = () =>  {
-        const { username, password, email} = this.state
+        const { username, password, password1, email, age, gender} = this.state
+        if (password===password1)   {
         this.props.history.push("/main", {
             username: username, 
             password: password,
+            age: age,
+            gender: gender,
             email: email,
             loggedIn: true
         });
+    } else {
+        this.setState({error: 'error'})
+    }
     }
 
-
     signUp = () =>   {
-        const { username, password, email } = this.state
+        const { username, password, password1, email, age, gender } = this.state
         return(
             <div>
                 Username
@@ -72,17 +83,35 @@ class SignUp extends Component {
                         value={password} 
                         onChange={(e)=> this.setInputValue('password', e.target.value)}>
                 </input>
-                {/* re-type Password
-                <input  placeholder="Password" 
+                <input  placeholder="re-enter Password" 
                         type="password" 
                         value={password1} 
-                        onChange={(e)=> this.setInputValue('password', e.target.value)}>
-                </input> */}
+                        onChange={(e)=> this.setInputValue('password1', e.target.value)}>
+                </input>
                 Email
                 <input  placeholder="e-mail" 
                         value={email} 
                         onChange={(e)=> this.setInputValue('email', e.target.value)}>
                 </input>
+                age
+                <input  placeholder="age"
+                        type= "number"  
+                        value={age} 
+                        onChange={(e)=> this.setInputValue('age', e.target.value)}>
+                </input>
+                gender
+                <form>
+                    <input  type="radio" 
+                            name="gender" 
+                            value="male"
+                            onClick={(e)=> this.setInputValue('gender', e.target.value)}/>
+                    male
+                    <input  type="radio" 
+                            name="gender" 
+                            value="female"
+                            onClick={(e)=> this.setInputValue('gender', e.target.value)}/>
+                    female
+                </form>
                 <button onClick={this.signingUp}>Sign-up</button>
                 <button onClick={this.login}>Already have an account?</button>
                 <div>{this.state.error}</div>
