5074918d6f82cc77eb0dddbc5617ad4fa5a6d4db
MtlRaze35 <marc.renaud35@gmail.com>
2018-02-26 18:55:21 -0500

    map directions/

--- FrontEnd/src/views/main/barList.js
+++ FrontEnd/src/views/main/barList.js
@@ -6,28 +6,48 @@ const BarList = styled.div`
   flex: 1;
   overflow-y: scroll;
   text-align: center;
+  padding: 20px;
 `;
 
-const BarItem = styled.div`
+const BarItem = styled(Button)`
+  margin: 10px;
   font-size: 14px;
   padding: .5rem;
   cursor: pointer;
+  transition: all 0.1s ease-in-out;
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
 
 class BarListComponent extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = { isActive: -1 };
+  }
+  handleClick = (venue, idx) => {
+    this.props.toggleInfoWindow(venue);
+    this.setState({ isActive: idx });
+  }
   render() { 
-    
     return (
       
       <BarList>
         {this.props.venues.map((venue, idx) => 
-        <BarItem key={idx}>        
-          <a 
-            onMouseEnter={ (e) => this.props.handleHover(e, venue, idx)}
-            onMouseOut={(e) => this.props.handleHoverOut(e, venue, idx)}
-            >
-            <Button onClick={() => this.props.toggleInfoWindow(venue)}>{venue.name}</Button>
-          </a>
+        <BarItem
+          key={idx}
+          onClick={() => this.handleClick(venue, idx)}
+          onMouseEnter={(e) => this.props.handleHover(e, venue, idx)}
+          onMouseOut={(e) => this.props.handleHoverOut(e, venue, idx)}
+          // highlight={venue.genderRatio}
+        >        
+          {venue.name}
+          <div>
+          {/* `https://www.google.com/maps?saddr=${this.props.userLoc}&daddr=${encodeURI(venue.vicinity)}` */}
+          {/* http://maps.google.com/?q=${encodeURI(venue.vicinity)} */}
+            <a href={`https://www.google.com/maps/dir/${this.props.userLoc.lat},${this.props.userLoc.lng}/${encodeURI(venue.vicinity)}`} 
+            target="_blank">{this.state.isActive === idx ? venue.vicinity : ''}</a>
+          </div>
         </BarItem>
         )}
       </BarList>
