6e3f3320c7328aee68780557cc094ff711034db6
MtlRaze35 <marc.renaud35@gmail.com>
2018-02-27 14:51:12 -0500

    sorter

--- FrontEnd/src/views/main/filter.js
+++ FrontEnd/src/views/main/filter.js
@@ -1,6 +1,27 @@
 import React from "react";
 import styled from 'styled-components';
-// import { Button } from '../styles';
+
+
+
+const Wrapper = styled.div`
+display: flex;
+padding: .2rem;
+  justify-content: space-between;
+  `
+
+  const Selectors = styled.select`
+  background-color: #404040;
+  color: white;
+  `
+
+  
+  const ToggleButton = styled.button`
+  
+  ${({ mapState }) => {
+    if (!mapState)  {return 'background-color: #404040; color: white'};
+    return 'background-color: #f2f2f2; color: black';
+  }};
+`;
 
 class SortComponent extends React.PureComponent {
   constructor() {
@@ -11,9 +32,9 @@ class SortComponent extends React.PureComponent {
     }
   }
   sendInfo = () => {
-
+    
     var placeIDs = []
-
+    
     this.props.venues.forEach((item, pos) => {
       if (item.place_id) {
         placeIDs.push(item.place_id)
@@ -21,37 +42,47 @@ class SortComponent extends React.PureComponent {
     });
     var infoToSend = { value: this.state.value, hiOrLow: this.state.hiOrLow, placeIDs: placeIDs }
     console.log(infoToSend)
+    fetch('/sort', {
+      method: 'Get'
+    }) .then(( response ) => response.json())
+      .then((data) => console.log(data))
+      .then(( data ) => this.props.setVenues(data))
     //Potential solution...
     //fetch sortedVenues and call this.props.setVenues to update parent state and replace venues array
   }
-
+  
   handleChange = (event) => {
     this.setState({ value: event.target.value });
   }
-
+  
   handleValueChange = (event) => {
     this.setState({ hiOrLow: event.target.value });
   }
-
+  
   render() {
     return (
-      <div>
+      <Wrapper>
         <div>
-          <button onClick={() => this.props.toggleMap()}>Toggle Map</button>
+          <ToggleButton 
+          mapState={this.props.mapState}
+          onClick={this.props.toggleMap}>Toggle Map</ToggleButton>
         </div>
         <div>
-          <select onChange={this.handleChange}>
+          <Selectors onChange={this.handleChange}>
             <option value="people"># of People</option>
             <option value="avgAge">Average Age</option>
             <option value="ratio">Male Ratio</option>
-          </select>
-          <select onChange={this.handleValueChange}>
+          </Selectors>
+          <Selectors onChange={this.handleValueChange}>
             <option value='high'>High</option>
             <option value='low'>Low</option>
-          </select>
-          <button onClick={this.sendInfo}>Filter</button>
+          </Selectors>
+          <button 
+          // setVenues={this.props.setVenues}
+          onClick={this.sendInfo}
+          >Filter</button>
         </div>
-      </div>
+      </Wrapper>
     )
   }
 }
